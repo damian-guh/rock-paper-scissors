37d1a8d6093fef2ab5cb3360bae539aeda2a2bfe
ThisGuh <jakiepolska1111@gmail.com>
2019-04-12 20:10:58 +0200

    1

--- main.js
+++ main.js
@@ -15,11 +15,23 @@ const handSelection = (event) => {
     hands.forEach(hand => hand.style.color = "");
     event.target.style.color = "#ccdb6b";
 }
+const checkResult = (player, ai) => {
+
+    if (player === ai) {
+
+    } else if ((player === "paper" && ai === "rock") || (player === "scissors" && ai === "paper") || (player === "rock" && ai === "scissors")) {
+
+    } else {
+
+    }
+}
+
 const startGame = () => {
     if (!game.playerChoice) {
         return alert("Choose hand !!!");
     }
     game.aiChoice = hands[Math.floor(Math.random() * 3)].dataset.option;
+    const gameResult = checkResult(game.playerChoice, game.aiChoice);
 }
 
 hands.forEach(hand => hand.addEventListener("click", handSelection));
